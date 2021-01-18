21994dc0308d3b452b8aea7f38d8d34cecfe9f51
YujiaCheng1996 <cyj@programmer.net>
2021-01-19 00:06:14 +0800

    init

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,29 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#KERNEL 5.4
+sed -i 's/KERNEL_PATCHVER:=4.19/KERNEL_PATCHVER:=5.4/g' target/linux/ipq40xx/Makefile
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+# 获取Lienol-package
+git clone https://github.com/Lienol/openwrt-package package/lienol
+# 获取luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/diy-packages/luci-app-serverchan
+# 获取koolproxyR并即时更新规则
+git clone https://github.com/YujiaCheng1996/luci-app-koolproxyR package/diy-packages/luci-app-koolproxyR
+pushd package/diy-packages/luci-app-koolproxyR
+./koolproxyupdate.sh
+popd
+#=================================================
+# 清除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+pushd package/lean
+# 清除旧版argon主题并拉取最新版
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config luci-app-argon-config
+# 清除旧版luci-app-adguardhome并拉取最新版
+rm -rf luci-app-adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome luci-app-adguardhome
+popd
